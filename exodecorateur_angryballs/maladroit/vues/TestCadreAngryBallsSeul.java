f4bd0c13c507485f1ff0ca847e94bdc6bf71c7a6
uchiwayas <yassine.tahri@outlook.fr>
2018-10-20 20:20:46 +0200

    Update qui marche (incomplet)

--- exodecorateur_angryballs/maladroit/vues/TestCadreAngryBallsSeul.java
+++ exodecorateur_angryballs/maladroit/vues/TestCadreAngryBallsSeul.java
@@ -1,22 +1,18 @@
 package exodecorateur_angryballs.maladroit.vues;
 
-import java.awt.Frame;
 import java.util.Vector;
 
-import exodecorateur_angryballs.maladroit.modele.Bille;
+import exodecorateur_angryballs.maladroit.modele.Objet;
 
+public class TestCadreAngryBallsSeul {
 
-public class TestCadreAngryBallsSeul
-{
-
-/**
- * @param args
- */
-public static void main(String[] args)
-{
-Vector<Bille> billes = new Vector<Bille>();
-CadreAngryBalls cadre =new CadreAngryBalls("angry balls", "animation de billes marrantes", billes);
-cadre.montrer();
-}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Vector<Objet> objets = new Vector<Objet>();
+		CadreAngryBalls cadre = new CadreAngryBalls("angry balls", "animation de billes marrantes", objets);
+		cadre.montrer();
+	}
 
 }
