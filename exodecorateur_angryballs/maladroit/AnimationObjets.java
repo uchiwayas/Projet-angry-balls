44fb04dc8c4f4ae8f68bedb12924141ee678bcbb
uchiwayas <yassine.tahri@outlook.fr>
2018-11-05 00:31:25 +0100

    Update exodecorateur_angryballs/maladroit/AnimationObjets.java

--- exodecorateur_angryballs/maladroit/AnimationObjets.java
+++ exodecorateur_angryballs/maladroit/AnimationObjets.java
@@ -50,7 +50,7 @@ public class AnimationObjets implements Runnable {
 				// les divisions par zéro
 
 				// System.err.println("deltaT = " + deltaT);
-				deltaT = 5;
+				deltaT = 1;
 
 				int i;
 				for (i = 0; i < objets.size(); ++i) // mise à jour de la liste
